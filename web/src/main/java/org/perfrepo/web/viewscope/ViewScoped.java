aa5851d659c8f04aaf3f6b420ecb5478d471576d
Jiri Holusa <jholusa@redhat.com>
2016-11-21 13:08:36 +0100

    Refactoring of UserService, split to GroupService + part of tests for UserService

--- web/src/main/java/org/perfrepo/web/viewscope/ViewScoped.java
+++ web/src/main/java/org/perfrepo/web/viewscope/ViewScoped.java
@@ -20,7 +20,9 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target({TYPE, METHOD, FIELD})
